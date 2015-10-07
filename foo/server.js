6f160228ab66805139a43ab0b486ae5d7215c952
danli <lidan1991bupt@gmail.com>
2015-10-07 15:55:50 +0200

    2. recursion - file validation + pipe writer

--- foo/server.js
+++ foo/server.js
@@ -7,42 +7,50 @@ var MIME = {
     '.js': 'application/javascript'
 };
 
-function combineFiles(pathnames, callback) {
-    var output = [];
+function outputFiles(pathnames, writer) {
+  (function next(i, len) {
+    if ( i < len) {
+      var reader = fs.createReadStream(pathnames[i]);
 
-    // async - array trasvers
-    (function next(i, len) {
-      if (i < len) {
-        // async - error handling
-        fs.readFile(pathnames[i], function (err, data) {
-          if (err) {
-            callback(err);
-          } else {
-              output.push(data);
-              next(i + i, len);
-          }
-        }); // end file read
-      } else {
-        callback(null, Buffer.concat(output));
-      }
-    } (0, pathnames.length));
+      reader.pipe(writer, {end: false});
+      reader.on('end', function() {
+        next(i + 1, len);
+      });
+    } else {
+      write.end();
+    }
+  } (0, pathnames.length));
 }
+
+function validateFiles(pathnames, callback) {
+  (function next(i, len) {
+    if (i < len) {
+      fs.stat(pathnames[i], function (err, stats) {
+                if (err) {
+                    callback(err);
+                } else if (!stats.isFile()) {
+                    callback(new Error());
+                } else {
+                    next(i + 1, len);
+                }
+            });
+        } else {
+            callback(null, pathnames);
+        }
+    }(0, pathnames.length));)
+}
+
 
 function main(argv) {
   var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8')),
       root = config.host;
-      console.log(root);
-      //port = config.port || 80;
 
   // create the server
   http.createServer(function (request, response) {
-    console.log(request.method);
-    console.log(request.headers);
-    console.log(request.url);
 
       var urlInfo = parseURL(root, request.url);
 
-      combineFiles(urlInfo.pathnames, function (err, data) {
+      validateFiles(urlInfo.pathnames, function (err, data) {
         if (err) {
           response.writeHead(404);
           response.end(err.message);
@@ -50,7 +58,7 @@ function main(argv) {
           response.writeHead(200, {
             'Content-Type': urlInfo.mime
           });
-          response.end(data);
+          outputFiles(pathnames, response);
         }
       });
   }).listen(8888);
